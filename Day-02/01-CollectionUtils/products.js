e9805f8ececc94901db716fe2f5a599ebede7444
tkmagesh <tkmagesh77@gmail.com>
2015-10-08 10:20:18 +0530

    Sort by comparer

--- Day-02/01-CollectionUtils/products.js
+++ Day-02/01-CollectionUtils/products.js
@@ -45,23 +45,48 @@ print("Sorting", function(){
     });
     print("Any collection by any attribute", function(){
         //modify the below function
-        function sort(){
-            for(var i=0; i<products.length-1; i++)
-                for(var j=i+1; j<products.length; j++){
-                    var left = products[i],
-                        right = products[j];
-                    if (left.id > right.id){
-                        products[i] = products[j];
-                        products[j] = left;
+        function sort(list, attrName){
+            for(var i=0; i<list.length-1; i++)
+                for(var j=i+1; j<list.length; j++){
+                    var left = list[i],
+                        right = list[j];
+                    if (left[attrName] > right[attrName]){
+                        list[i] = list[j];
+                        list[j] = left;
                     }
                 }
         }
         print("products by cost", function(){
-            //do the sorting
+            sort(products, "cost");
             console.table(products);
         })
         print("products by units", function(){
-            //do the sorting
+            sort(products, "units");
+            console.table(products);
+        })
+    });
+     print("Any collection by any comparer", function(){
+        //modify the below function
+        function sort(list, compareFn){
+            for(var i=0; i<list.length-1; i++)
+                for(var j=i+1; j<list.length; j++){
+                    var left = list[i],
+                        right = list[j];
+                    if (compareFn(left, right) > 0){
+                        list[i] = list[j];
+                        list[j] = left;
+                    }
+                }
+        }
+        print("products by value [ units * cost ]", function(){
+            var productComparerByValue = function(p1, p2){
+                var p1Value = p1.units * p1.cost,
+                    p2Value = p2.units * p2.cost;
+                if (p1Value < p2Value) return -1;
+                if (p1Value === p2Value) return 0;
+                return 1;
+            }
+            sort(products, productComparerByValue);
             console.table(products);
         })
     });
